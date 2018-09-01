875f79438d2ed4874938bde81ef304237296ab1a
IES <IES@IES-PC>
2018-09-01 17:31:17 -0300

    cert

--- IpContextInfo/src/main/java/com/meli/ipcontextinfo/Application.java
+++ IpContextInfo/src/main/java/com/meli/ipcontextinfo/Application.java
@@ -1,5 +1,6 @@
 package com.meli.ipcontextinfo;
 
+import java.net.URL;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,8 +10,14 @@ import org.springframework.cache.annotation.EnableCaching;
 public class Application {
 
     public Application() {
+
+        // add the valid certificates 
+        URL ul = Application.class.getClassLoader().getResource("cacerts");
+        System.setProperty("javax.net.ssl.trustStore", ul.getPath());
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+
     }
-    
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
